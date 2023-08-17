9f52ebf47f0892cfc0d6d09401d4c735b07e0fe6
Noddy30 <neduan41@gmail.com>
2023-08-17 20:56:01 +0200

    Init Commit

---
+++ gustav/DataDataAccess/BaseModel (copy).cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace gustav.Models
+{
+    public class BaseModel
+    {
+        public bool isDeleted { get; set; }
+        public DateTime CreatedTimestamp = DateTime.Now;
+        public string Id = Guid.NewGuid().ToString();
+    }
+}
+
